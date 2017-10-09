3db30d75a023ab6a6b5b24d358c8aed316b51967
Hugo Alberto Ramírez <hugo@empowerdata.mx>
2017-10-09 18:38:25 -0500

    Added KeyPhrases extraction, opinions extraction
    llamadas AJAX a todas las APIS y contruccion de dos JSON

--- upload.js
+++ upload.js
@@ -1,3 +1,10 @@
+var SpeechToTextKey = "9fc280924bed46b9ab0c3714ca3069ca";
+var TextAnalysisKey = "9c0bc0190edf451fa24029d7c2419210";
+
+var nFiles = 0;
+var keyPhrasesJSON = {};
+var opinionsJSON = {};
+
 $('.upload-btn').on('click', function (){
     $('#upload-input').click();
     $('.progress-bar').text('0%');
@@ -8,12 +15,10 @@ $('#upload-input').on('change', function(){
 
   var files = $(this).get(0).files;
 
+  nFiles = files.length;
   if (files.length > 0){
     
     require(["Speech.Browser.Sdk"], function(SDK) {
-
-      var subscriptionKey = "9fc280924bed46b9ab0c3714ca3069ca";
-
       var recognizerConfig = new SDK.RecognizerConfig(
         new SDK.SpeechConfig(
             new SDK.Context(
@@ -23,114 +28,117 @@ $('#upload-input').on('change', function(){
         "es-ES", 
         SDK.SpeechResultFormat["Simple"]); // Detailed
       
-      var authentication = new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
+      var authentication = new SDK.CognitiveSubscriptionKeyAuthentication(SpeechToTextKey);
       
-      for (var i = 0; i < files.length; i++) {
-        //formData.append('uploads', file, file.name);
-
+      for (var i = 0; i < nFiles; i++) {
         var file = files[i];
-
         recognizer = SDK.CreateRecognizerWithFileAudioSource(recognizerConfig, authentication, file);
-        RecognizerStart(SDK, recognizer, file); 
+        RecognizerStart(SDK, recognizer, file.name, i); 
       }
     });
-
   }
 });
 
-function RecognizerStart(SDK, recognizer, file) {
+function RecognizerStart(SDK, recognizer, fileName, index) {
   recognizer.Recognize((event) => {
-      switch (event.Name) {
-          case "RecognitionTriggeredEvent" :
-              UpdateStatus(file, event.Name, "Initializing");
-              break;
-          case "ListeningStartedEvent" :
-              UpdateStatus(file, event.Name, "Listening");
-              break;
-          case "RecognitionStartedEvent" :
-              UpdateStatus(file, event.Name, "Listening_Recognizing");
-              break;
-          case "SpeechStartDetectedEvent" :
-              UpdateStatus(file, event.Name, "Listening_DetectedSpeech_Recognizing");
-              console.log(JSON.stringify(event.Result)); 
-              break;
-          case "SpeechHypothesisEvent" :
-              UpdateStatus(file, event.Name, event.Result.Text);
-              UpdateStatus(file, event.Name, JSON.stringify(event.Result));
-              break;
-          case "SpeechEndDetectedEvent" :
-              OnSpeechEndDetected(file);
-              UpdateStatus(file, event.Name, "Processing_Adding_Final_Touches");
-              console.log(JSON.stringify(event.Result)); 
-              break;
-          case "SpeechSimplePhraseEvent" :
-              //UpdateStatus(file, event.Name, "SpeechSimplePhraseEvent");
-              //UpdateStatus(file, event.Name, JSON.stringify(event.Result.DisplayText, null, 3));
+    if (event.Name == "SpeechSimplePhraseEvent")
+    {
+      console.log(event.Result.DisplayText);
+      
+      var documents = { documents: [ { id: '1', language: 'es', text: event.Result.DisplayText},] };
 
-              var documents = 
-              { 
-                documents: [
-                 { 
-                   id: '1', 
-                   language: 'es', 
-                   text: event.Result.DisplayText
-                 },
-               ]
-              };
+      GetScore(documents, fileName);
+      GetKeyPhrases(documents);
+    }   
+    else if (event.Name == "RecognitionEndedEvent")
+    {
+      if (index == nFiles - 1)
+      {
+        console.log("============= keyPhrasesJSON =============");
+        console.log(JSON.stringify(keyPhrasesJSON));
+        // for(key in keyPhrasesJSON){
+        //   var value = keyPhrasesJSON[key];
+        //   console.log(key + ": " + value)
+        // }
 
-              var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
-                "method": "POST",
-                "headers": {
-                  "ocp-apim-subscription-key": "9c0bc0190edf451fa24029d7c2419210",
-                  "content-type": "application/json",
-                  "cache-control": "no-cache",
-                  "postman-token": "168d5d45-71a0-7e2c-59fa-6025967c1e55"
-                },
-                "data": JSON.stringify(documents)
-              }
-              
-              $.ajax(settings).done(function (response) {
-                console.log(event.Result.DisplayText);
-                console.log(response.documents[0].score);
-              });
-              
-              break;
-          case "SpeechDetailedPhraseEvent" :
-              UpdateStatus(file, event.Name, "SpeechDetailedPhraseEvent");
-              UpdateStatus(file, event.Name, JSON.stringify(event.Result, null, 3));
-              break;
-          case "RecognitionEndedEvent" : 
-              OnComplete();
-              UpdateStatus(file, "END", JSON.stringify(event));
-              break;
-          default:
-              console.log(JSON.stringify(event)); 
+        console.log("============= opinionsJSON =============");
+        console.log(JSON.stringify(opinionsJSON));
+        // for(key in opinionsJSON){
+        //   var value = opinionsJSON[key];
+        //   console.log(key + ": " + value)
+        // }
       }
+    }
   })
-  .On(() => {
-      // The request succeeded. Nothing to do here.
-  },
-  (error) => {
-      console.error(error);
+  .On(
+    () => { },
+    (error) => { console.error(error);}
+  );
+}
+
+function GetScore(documents, fileName)
+{
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+    "method": "POST",
+    "headers": {
+      "ocp-apim-subscription-key": TextAnalysisKey,
+      "content-type": "application/json",
+      "cache-control": "no-cache"
+    },
+    "data": JSON.stringify(documents)
+  }
+  
+  $.ajax(settings).done(function (response) {
+    if (response.documents && response.documents[0] && response.documents[0].score)
+    {
+      var score = response.documents[0].score;
+      
+      if (opinionsJSON[fileName])
+      {
+        var arr = opinionsJSON[fileName];
+        arr.push(score);
+      }
+      else
+      {
+        opinionsJSON[fileName] = [score];
+      }
+    }
   });
 }
 
-function UpdateStatus(file, event, text) {
-  //console.log(file.name + ' >>> ' + event + ' >>> \n' + text);
-}
+function GetKeyPhrases(documents)
+{
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases",
+    "method": "POST",
+    "headers": {
+      "ocp-apim-subscription-key": TextAnalysisKey,
+      "content-type": "application/json",
+      "cache-control": "no-cache"
+    },
+    "data": JSON.stringify(documents)
+  }
 
-function UpdateRecognizedHypothesis(text) {
-  //hypothesisDiv.innerHTML = text;
-  console.log(text + "\n");
-}
-
-function OnSpeechEndDetected(file) {
-  // evento que se dispara cuando termina la detección de un archivo
-}
-
-function OnComplete() {
-  console.log("Complete !");
+  $.ajax(settings).done(function (response) {
+    if (response.documents && response.documents[0] && response.documents[0].keyPhrases)
+    {
+      var keyPhrases = response.documents[0].keyPhrases;
+      for (var i = 0; i < keyPhrases.length; i++)
+      {
+        if (keyPhrasesJSON[keyPhrases[i]])
+        {
+          keyPhrasesJSON[keyPhrases[i]] = keyPhrasesJSON[keyPhrases[i]] + 1;
+        }
+        else
+        {
+          keyPhrasesJSON[keyPhrases[i]] = 1;
+        }
+      }
+    }
+  });
 }
